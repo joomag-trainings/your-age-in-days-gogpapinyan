e4c32131db5eb5185bd3c67506615878a8e48d2d
Gohar <goharpapinyan@gmail.com>
2017-07-10 23:25:17 +0400

    How many days are you old?.Style changed

--- ageindays.php
+++ ageindays.php
@@ -1,11 +1,11 @@
 <?php
-    date_default_timezone_set("Europe/Moscow");
-    echo "Your time zone is " . date_default_timezone_get().   "<br />";
-    $time = time();
-    echo "Current date is ". date("D m y H:i:s"). "<br />";
-    $yourBirthDate = "20 September 1996 01:00";
-    echo "Your birth date is $yourBirthDate <br />";
-    $birthDate = strtotime($yourBirthDate);
-    $ageInDays = ceil(($time - $birthDate)/60/60/24);
-    echo "You're $ageInDays days old";
+date_default_timezone_set("Europe/Moscow");
+echo "Your time zone is ", date_default_timezone_get(), "<br />";
+$time = time();
+echo "Current date is ", date("D m y H:i:s"), "<br />";
+$yourBirthDate = "20 September 1996 01:00";
+echo "Your birth date is ", $yourBirthDate, "<br />";
+$birthDate = strtotime($yourBirthDate);
+$ageInDays = ceil(($time - $birthDate)/60/60/24);
+echo "You're $ageInDays days old";
 
