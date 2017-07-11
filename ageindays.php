ad48c14c35814e0b14703ed80452896b6c587f12
Gohar <goharpapinyan@gmail.com>
2017-07-11 17:11:27 +0400

    Reformatted version

--- ageindays.php
+++ ageindays.php
@@ -6,6 +6,6 @@ echo "Current date is ", date("D m y H:i:s"), "<br />";
 $yourBirthDate = "20 September 1996 01:00";
 echo "Your birth date is ", $yourBirthDate, "<br />";
 $birthDate = strtotime($yourBirthDate);
-$ageInDays = ceil(($time - $birthDate)/60/60/24);
+$ageInDays = ceil(($time - $birthDate) / 60 / 60 / 24);
 echo "You're $ageInDays days old";
 
